205fed4567454afc658872f93b9920af27356cbf
Travis Deployment <noreply@travis-ci.org>
2016-09-21 08:37:34 +0000

    Auto-deploy via Travis CI

--- build/all.js
+++ build/all.js
@@ -17788,25 +17788,25 @@ var wdcw = window.wdcw || {};
 
 /*jshint -W082 */
 
-var module=module || {}
+var module = module || {}
 
-,
-window=window || {}
-,
+  ,
+  window = window || {}
+  ,
 
-document=document || { location:{search:"" }}
-,
-jQuery=jQuery || {}
+  document = document || {location: {search: ""}}
+  ,
+  jQuery = jQuery || {}
 
-,
-tableau=tableau || {}
+  ,
+  tableau = tableau || {}
 
-,
-wdcw=window.wdcw || {}
+  ,
+  wdcw = window.wdcw || {}
 
-;
-module.exports=function($, tableau, wdcw) {
-    /**
+  ;
+module.exports = function ($, tableau, wdcw) {
+  /**
    * Run during initialization of the web data connector.
    *
    * @param {string} phase
@@ -17824,28 +17824,31 @@ module.exports=function($, tableau, wdcw) {
    *   A callback function that you must call when all setup tasks have been
    *   performed.
    */
-    wdcw.setup=function setup(phase, setUpComplete) {
-        // You may need to perform set up or other initialization tasks at various
-        // points in the data connector flow. You can do so here.
-        switch (phase) {
-            case tableau.phaseEnum.interactivePhase: // Perform set up tasks that relate to when the user will be prompted to
-            // enter information interactively.
-            break;
-            case tableau.phaseEnum.gatherDataPhase: // Perform set up tasks that should happen when Tableau is attempting to
-            // retrieve data from your connector (the user is not prompted for any
-            // information in this phase.
-            break;
-            case tableau.phaseEnum.authPhase: // Perform set up tasks that should happen when Tableau is attempting to
-            // refresh OAuth authentication tokens.
-            break;
-        }
-        // Always register when initialization tasks are complete by calling this.
-        // This can be especially useful when initialization tasks are asynchronous
-        // in nature.
-        setUpComplete();
+  wdcw.setup = function setup(phase, setUpComplete) {
+    // You may need to perform set up or other initialization tasks at various
+    // points in the data connector flow. You can do so here.
+    switch (phase) {
+      case tableau.phaseEnum.interactivePhase: // Perform set up tasks that relate to when the user will be prompted to
+        // enter information interactively.
+        tableau.log("Interactive Phase");
+        break;
+      case tableau.phaseEnum.gatherDataPhase: // Perform set up tasks that should happen when Tableau is attempting to
+        // retrieve data from your connector (the user is not prompted for any
+        // information in this phase.
+        tableau.log("GatherData Phase");
+        break;
+      case tableau.phaseEnum.authPhase: // Perform set up tasks that should happen when Tableau is attempting to
+        // refresh OAuth authentication tokens.
+        tableau.log("Auth Phase");
+        break;
     }
-    ;
-    /**
+    // Always register when initialization tasks are complete by calling this.
+    // This can be especially useful when initialization tasks are asynchronous
+    // in nature.
+    setUpComplete();
+  }
+  ;
+  /**
    * Run when the web data connector is being unloaded. Useful if you need
    * custom logic to clean up resources or perform other shutdown tasks.
    *
@@ -17853,13 +17856,13 @@ module.exports=function($, tableau, wdcw) {
    *   A callback function that you must call when all shutdown tasks have been
    *   performed.
    */
-    wdcw.teardown=function teardown(tearDownComplete) {
-        // Once shutdown tasks are complete, call this. Particularly useful if your
-        // clean-up tasks are asynchronous in nature.
-        tearDownComplete();
-    }
-    ;
-    /**
+  wdcw.teardown = function teardown(tearDownComplete) {
+    // Once shutdown tasks are complete, call this. Particularly useful if your
+    // clean-up tasks are asynchronous in nature.
+    tearDownComplete();
+  }
+  ;
+  /**
    * Primary method called when Tableau is asking for the column headers that
    * this web data connector provides. Takes a single callable argument that you
    * should call with the headers you've retrieved.
@@ -17885,60 +17888,64 @@ module.exports=function($, tableau, wdcw) {
    *     {name: 'DateTime Column', type: 'datetime', incrementalRefresh: true}
    *   ]);
    */
-    wdcw.columnHeaders=function columnHeaders(registerHeaders) {
-        // Access your input option like this to tweak data gathering logic.
-        var collectionGUID = this.getConnectionData()['CollectionGUID'];
-        var reportGUID = this.getConnectionData()['ReportGUID'];
-        var accountGUID = this.getConnectionData()['AccountGUID'];
-        var beginDate = this.getConnectionData()['begin'];
-        var endDate = this.getConnectionData()['end'];
-        var apiServer = getQSByKey('APIServer');
-        var username = this.getUsername();
-        var password = this.getPassword();
+  wdcw.columnHeaders = function columnHeaders(registerHeaders) {
+    // Access your input option like this to tweak data gathering logic.
+    var dataExportGUID = this.getConnectionData()['DataExportGUID'];
+    var accountGUID = this.getConnectionData()['AccountGUID'];
+    var beginDate = this.getConnectionData()['begin'];
+    var endDate = this.getConnectionData()['end'];
+    var dateRange = this.getConnectionData()['daterange'];
+    var apiServer = getQSByKey('APIServer');
+    var username = this.getUsername();
+    var password = this.getPassword();
 
-        // Do the same to retrieve your actual data.
-        $.ajax( {
-            url: buildApiFrom('v1/account/'+accountGUID+'/report/'+reportGUID, {
-                server: apiServer
-            }
-            ), // Add basic authentication headers to your request like this. Note that
-            // the password is encrypted when stored by Tableau; the username is not.
-            xhrFields: {
-              withCredentials: true
-            },
-            headers: {
-              "Authorization": "Basic " + btoa(username +":"+ password)
-            },
-            crossDomain: true,
-            success: function dataRetrieved(response) {
-                var processedColumns=[], dimensions, measures;
-                // If necessary, process the response from the API into the expected
-                // format (highlighted below):
-                $.each(response.dimension, function( index, value ) {
-                  processedColumns.push( {
-                      name: value.name, type: "string", // If your connector supports incremental extract refreshes, you
-                      // can indicate the column to use for refreshing like this:
-                      incrementalRefresh: false
-                  })
-                });
-
-                $.each(response.measure, function( index, value ) {
-                  processedColumns.push( {
-                      name: value.name, type: "float", // If your connector supports incremental extract refreshes, you
-                      // can indicate the column to use for refreshing like this:
-                      incrementalRefresh: false
-                  })
-                });
-
-                // Once data is retrieved and processed, call registerHeaders().
-                registerHeaders(processedColumns);
-            }, // Use this.ajaxErrorHandler for basic error handling.
-            error: this.ajaxErrorHandler
+    // Do the same to retrieve your actual data.
+    $.ajax({
+      url: buildApiFrom('v1/account/' + accountGUID + '/export/' + dataExportGUID, {
+          server: apiServer
         }
-        );
-    }
-    ;
-    /**
+      ), // Add basic authentication headers to your request like this. Note that
+      // the password is encrypted when stored by Tableau; the username is not.
+      xhrFields: {
+        withCredentials: true
+      },
+      headers: {
+        "Authorization": "Basic " + btoa(username + ":" + password)
+      },
+      crossDomain: true,
+      success: function dataRetrieved(response) {
+
+        tableau.log(response);
+
+        var processedColumns = [], dimensions, measures;
+        // If necessary, process the response from the API into the expected
+        // format (highlighted below):
+
+
+        $.each(response.report.dimension, function (index, value) {
+          processedColumns.push({
+            name: value.name, type: "string", // If your connector supports incremental extract refreshes, you
+            // can indicate the column to use for refreshing like this:
+            incrementalRefresh: false
+          })
+        });
+
+        $.each(response.report.measure, function (index, value) {
+          processedColumns.push({
+            name: value.name, type: "float", // If your connector supports incremental extract refreshes, you
+            // can indicate the column to use for refreshing like this:
+            incrementalRefresh: false
+          })
+        });
+
+        // Once data is retrieved and processed, call registerHeaders().
+        registerHeaders(processedColumns);
+      }, // Use this.ajaxErrorHandler for basic error handling.
+      error: this.ajaxErrorHandler
+    });
+  }
+  ;
+  /**
    * Primary method called when Tableau is asking for your web data connector's
    * data. Takes a callable argument that you should call with all of the
    * data you've retrieved. You may optionally pass a token as a second argument
@@ -17974,84 +17981,99 @@ module.exports=function($, tableau, wdcw) {
    *   be passed as the value of lastRecord when an incremental refresh is
    *   triggered.
    */
-    wdcw.tableData=function tableData(registerData, lastRecord) {
-        // Access your input option like this to tweak data gathering logic.
-        var collectionGUID=this.getConnectionData()['CollectionGUID'];
-        var reportGUID=this.getConnectionData()['ReportGUID'];
-        var accountGUID=this.getConnectionData()['AccountGUID'];
-        var beginDate = this.getConnectionData()['begin'];
-        var endDate = this.getConnectionData()['end'];
-        var apiServer=getQSByKey('APIServer');
+  wdcw.tableData = function tableData(registerData, lastRecord) {
+    // Access your input option like this to tweak data gathering logic.
+    var dataExportGUID = this.getConnectionData()['DataExportGUID'];
+    var accountGUID = this.getConnectionData()['AccountGUID'];
+    var beginDate = this.getConnectionData()['begin'];
+    var dateRange = this.getConnectionData()['daterange'];
+    var timezone = this.getConnectionData()['timezone'];
+    var endDate = this.getConnectionData()['end'];
+    var apiServer = getQSByKey('APIServer');
 
-        var username = this.getUsername();
-        var password = this.getPassword();
+    var username = this.getUsername();
+    var password = this.getPassword();
 
-        // Do the same to retrieve your actual data.
-        $.ajax( {
-            url: buildApiFrom('v1/account/'+accountGUID+'/collections/'+collectionGUID+'/reports/'+reportGUID+'/data', {
-                last: lastRecord, server: apiServer, begin: beginDate, end: endDate
-            }),
-            xhrFields: {
-              withCredentials: true
-            },
-            headers: {
-              "Authorization": "Basic " + btoa(username +":"+ password)
-            },
-            crossDomain: true,
-            success: function dataRetrieved(response) {
-                var processedData=[]; // Determine if more data is available via paging.
-                //moreData=false;
-                // You may need to perform processing to shape the data into an array of
-                // objects where each object is a map of column names to values.
+    getTableDataAjax(this);
 
-                $.each(response.dimensions, function( index, value ) {
-                  processedData = buildDimMeasureMap(value, processedData);
-                });
+    // Do the same to retrieve your actual data.
+    function getTableDataAjax(_this) {
+      var request = $.ajax({
+        url: buildApiFrom('v1/account/' + accountGUID + '/dataexport/' + dataExportGUID + '/data', {
+          last: lastRecord, server: apiServer, begin: beginDate, end: endDate, dateRange: dateRange, timezone: timezone
+        }),
+        xhrFields: {
+          withCredentials: true
+        },
+        headers: {
+          "Authorization": "Basic " + btoa(username + ":" + password)
+        },
+        crossDomain: true,
+        success: function dataRetrieved(response) {
 
-                // Once you've retrieved your data and shaped it into the form expected,
-                // call the registerData function. If more data can be retrieved, then
-                // supply a token to inform further paged requests.
-                // @see buildApiFrom()
-                //if (moreData) {
-                //    registerData(processedData, response.meta.page);
-                //}
-                // Otherwise, just register the response data with the callback.
-                //else {
-                    registerData(processedData);
-                //}
-            }
-            , // Use this.ajaxErrorHandler for basic error handling.
-            error: this.ajaxErrorHandler
+          if (request.status == 202) {
+            tableau.log("Data not ready, retrying...");
+            tableau.log(response);
+            //retry
+            setTimeout(1000, getTableDataAjax(_this));
+            return;
+          }
+
+          tableau.log(response);
+          var processedData = []; // Determine if more data is available via paging.
+          //moreData=false;
+          // You may need to perform processing to shape the data into an array of
+          // objects where each object is a map of column names to values.
+
+          $.each(response.dimensions, function (index, value) {
+            processedData = buildDimMeasureMap(value, processedData);
+          });
+
+          // Once you've retrieved your data and shaped it into the form expected,
+          // call the registerData function. If more data can be retrieved, then
+          // supply a token to inform further paged requests.
+          // @see buildApiFrom()
+          //if (moreData) {
+          //    registerData(processedData, response.meta.page);
+          //}
+          // Otherwise, just register the response data with the callback.
+          //else {
+          registerData(processedData);
+          //}
         }
-        );
+        , // Use this.ajaxErrorHandler for basic error handling.
+        error: _this.ajaxErrorHandler
+      });
     }
-    ;
-    // You can write private methods for use above like this:
+  }
+  ;
+  // You can write private methods for use above like this:
 
-    /**
-    * Helper to build the structure for the dimension
-    **/
-    function buildDimMeasureMap(dimension, dataMap, currentData) {
-      var data = new Object();
-      if (currentData) {
-        data = $.extend({}, currentData);
-      }
+  /**
+   * Helper to build the structure for the dimension
+   **/
+  function buildDimMeasureMap(dimension, dataMap, currentData) {
+    var data = new Object();
+    if (currentData) {
+      data = $.extend({}, currentData);
+    }
 
-      data[dimension.type] = dimension.value;
-      $.each(dimension.dimensions, function(index, value) {
-        dataMap = buildDimMeasureMap(value, dataMap, data);
-      });
+    data[dimension.type] = dimension.value;
+    $.each(dimension.dimensions, function (index, value) {
+      dataMap = buildDimMeasureMap(value, dataMap, data);
+    });
 
-      $.each(dimension.measures, function (index, value) {
-        data[value.name] = value.value;
-      });
+    $.each(dimension.measures, function (index, value) {
+      data[value.name] = value.value;
+      tableau.log(value.name + ": " + value.value);
+    });
 
-      //single level report only && not top level wrapper object
-      if ((!currentData && !dimension.dimensions) || currentData) {
-        dataMap.push( data );
-      }
-      return dataMap;
-    };
+    //single level report only && not top level wrapper object
+    if ((!currentData && !dimension.dimensions) || currentData) {
+      dataMap.push(data);
+    }
+    return dataMap;
+  };
 
   /**
    * Helper function to build an API endpoint.
@@ -18062,17 +18084,9 @@ module.exports=function($, tableau, wdcw) {
    * @param {object} opts
    *   Options to inform query parameters and paging.
    */
-    function buildApiFrom(path, opts) {
-        opts=opts || {};
+  function buildApiFrom(path, opts) {
+    opts = opts || {};
 
-        var server=opts.server || "https://api.webtrends.io/";
-        path=server + path;
-        if (opts.begin) {
-          path += "?begin=" + opts.begin + "/00";
-          path += "&end=" + opts.end + "/23";
-        }
-
-        return path;
     }
 
     /**
@@ -18111,12 +18125,33 @@ module.exports=function($, tableau, wdcw) {
                 block=block << 8 | charCode;
             }
             return output;
+      function InvalidCharacterError(message) {
+        this.message = message;
+      }
+
+      InvalidCharacterError.prototype = new Error;
+      InvalidCharacterError.prototype.name = 'InvalidCharacterError';
+      for ( // initialize result and counter
+        var block, charCode, idx = 0, map = chars, output = '';
+        // if the next str index does not exist:
+        //   change the mapping table to "="
+        //   check if d has no fractional digits
+        str.charAt(idx | 0) || (map = '=', idx % 1);
+        // "8 - idx % 1 * 8" generates the sequence 2, 4, 6, 8
+        output += map.charAt(63 & block >> 8 - idx % 1 * 8)) {
+        charCode = str.charCodeAt(idx += 3 / 4);
+        if (charCode > 0xFF) {
+          throw new InvalidCharacterError("'btoa' failed: The string to be encoded contains characters outside of the Latin1 range.");
         }
+        block = block << 8 | charCode;
+      }
+      return output;
     }
-    /* jshint ignore:end */
-    return wdcw;
+  }
+  /* jshint ignore:end */
+  return wdcw;
 }
 
 ;
 // Set the global wdcw variable as expected.
-wdcw=module.exports(jQuery, tableau, wdcw);
+wdcw = module.exports(jQuery, tableau, wdcw);
